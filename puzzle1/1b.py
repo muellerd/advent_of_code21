13dd87047b30b618c0e31ee071970a8136ed5adf
muellerd <info@daniel-mueller.org>
2021-12-01 10:31:40 +0100

    Initial commit, Day 1

---
+++ puzzle1/1b.py
@@ -0,0 +1,33 @@
+input  = "input1.txt"
+
+depthReadings = []
+changes = []        # 1 = increase, 0 = no change, -1 = decrease
+
+with open(input) as f:
+    for l in f:
+        depthReadings.append(int(l.strip()))
+
+i = 3
+changes.append(0)
+while i < len(depthReadings):
+    if (depthReadings[i] + depthReadings[i-1] + depthReadings[i-2]) \
+            < (depthReadings[i-1] + depthReadings[i-2] + depthReadings[i-3]):
+        changes.append(-1)
+    if (depthReadings[i] + depthReadings[i-1] + depthReadings[i-2]) \
+           > (depthReadings[i-1] + depthReadings[i-2] + depthReadings[i-3]):
+        changes.append(1)
+    if (depthReadings[i] + depthReadings[i-1] + depthReadings[i-2]) \
+            == (depthReadings[i-1] + depthReadings[i-2] + depthReadings[i-3]):
+        changes.append(0)
+
+    i += 1
+
+# count
+counts = {}
+for change in changes:
+    if change not in counts:
+        counts[change] = 1
+    else:
+        counts[change] += 1
+
+print(counts)
